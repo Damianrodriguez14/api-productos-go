49bea2d8339bd6320cc83c24223c6c52c1d906b3
Damian Rodriguez <damianezequielrodriguez14@gmail.com>
2026-06-07 20:45:32 -0300

    agrego frontend y cors

--- main.go
+++ main.go
@@ -22,6 +22,19 @@ type Producto struct {
 
 var db *sql.DB
 
+func corsMiddleware(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+		next(w, r)
+	}
+}
+
 func main() {
 	godotenv.Load()
 
@@ -40,8 +53,8 @@ func main() {
 
 	crearTabla()
 
-	http.HandleFunc("/productos", manejarProductos)
-	http.HandleFunc("/productos/", manejarProductoPorID)
+	http.HandleFunc("/productos", corsMiddleware(manejarProductos))
+	http.HandleFunc("/productos/", corsMiddleware(manejarProductoPorID))
 
 	log.Println("API corriendo en http://localhost:8080")
 	http.ListenAndServe(":8080", nil)
